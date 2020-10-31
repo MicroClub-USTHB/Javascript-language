f5b0dec1c74b7dce5e2efb73e14463358805ffac
Abdelhak Ihadjadene <xenomshox69@gmail.com>
2020-10-31 19:59:51 +0100

    Added BFS Algorithm

--- algorithms/data-structures/BinarySearchTree.js
+++ algorithms/data-structures/BinarySearchTree.js
@@ -65,6 +65,20 @@ class BinarySearchTree {
         }
         return false;
     }
+    BFS() {
+        let node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
 }
 
 //      10
